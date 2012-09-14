6fe146835f166f04d896c39b3b3bb631123c5b64
Daniel Rus Morales <danirus@eml.cc>
2012-09-15 00:32:29 +0200

    Add new diary's templatetag and rename older.
    Renames get_latest_diary_entries to get_latest_diary_days, it fits
    better what it does (it retrieve N days from the diary).
    Creates a new templatetag: get_latest_diary_entries. It retrieves the
    last N entries from the diary.
    To distinguish both templatetags, think that the first retrieves
    days where each day may have variable number of entries, and the later
    retrieves the last N entries.

--- dress_blog/templatetags/diary.py
+++ dress_blog/templatetags/diary.py
@@ -7,11 +7,12 @@ from django.db import models
 import re
 
 Diary = models.get_model('dress_blog', 'diary')
+DiaryDetail = models.get_model('dress_blog', 'diarydetail')
 
 register = template.Library()
 
 
-class LatestDiaryEntries(template.Node):
+class LatestDiaryDays(template.Node):
     def __init__(self, limit, var_name):
         try:
             self.limit = int(limit)
@@ -29,10 +30,52 @@ class LatestDiaryEntries(template.Node):
         return ''
 
 
+class LatestDiaryEntries(template.Node):
+    def __init__(self, limit, var_name):
+        try:
+            self.limit = int(limit)
+        except:
+            self.limit = template.Variable(limit)
+        self.var_name = var_name
+
+    def render(self, context):
+        if not isinstance(self.limit, int):
+            self.limit = int( self.limit.resolve(context) )
+
+        entries = DiaryDetail.objects.published().order_by("-pub_date")[:self.limit]
+        if entries:
+            context[self.var_name] = entries
+        return ''
+
+
+@register.tag
+def get_latest_diary_days(parser, token):
+    """
+    Gets any number of latest daily diary instances and stores them in a variable.
+
+    Syntax::
+
+        {% get_latest_diary_days [days] as [var_name] %}
+
+    Example usage::
+
+        {% get_latest_diary_days 2 as latest_in_diary %}
+    """
+    try:
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError, "%s tag requires arguments" % token.contents.split()[0]
+    m = re.search(r'(.*?) as (\w+)', arg)
+    if not m:
+        raise template.TemplateSyntaxError, "%s tag had invalid arguments" % tag_name
+    format_string, var_name = m.groups()
+    return LatestDiaryDays(format_string, var_name)
+
+
 @register.tag
 def get_latest_diary_entries(parser, token):
     """
-    Gets any number of latest daily diary instances and stores them in a variable.
+    Gets any number of latest diary entries and stores them in a variable.
 
     Syntax::
 
@@ -40,7 +83,7 @@ def get_latest_diary_entries(parser, token):
 
     Example usage::
 
-        {% get_latest_diary_entries 2 as latest_diary_entry_list %}
+        {% get_latest_diary_entries 2 as latest_in_diary %}
     """
     try:
         tag_name, arg = token.contents.split(None, 1)
