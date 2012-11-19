329eb053ce67c1f62370b3da55720a1e1a4365d2
Daniel Rus Morales <danirus@eml.cc>
2012-11-19 22:14:10 +0100

    Fix datetime field initialization and demo site

--- example/demo/install.sh
+++ example/demo/install.sh
@@ -4,5 +4,6 @@ python manage.py syncdb --noinput
 python manage.py migrate django_comments_xtd
 python manage.py migrate flatblocks
 python manage.py loaddata ../../dress_blog/fixtures/flatblocks.json
+python manage.py loaddata ../../dress_blog/fixtures/initial_data.json
 python manage.py loaddata fixtures/django_comments_xtd.json
 python afterload.py
